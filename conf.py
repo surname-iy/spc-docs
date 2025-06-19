3e9c7dc01de2ec8dfa21cd5fe786c881b08c888d
surname-iy <827148614@qq.com>
2025-06-19 17:01:36 +0800

    “修复构建问题234”

--- conf.py
+++ conf.py
@@ -1,15 +1,19 @@
-# conf.py
+# -*- coding: utf-8 -*-
 project = 'SPC 系统文档'
 author = '你的名字'
 version = '1.0'
 release = '1.0'
 
+# 文档源目录（Sphinx 会从 docs/ 读取 RST 文件）
+source_dir = 'docs'  
 source_suffix = '.rst'
-master_doc = 'docs/index'  # 主文档路径
+
+# 主文档是 docs/index.rst（只需指定文件名，Sphinx 从 source_dir 查找）
+master_doc = 'index'  
 
 extensions = [
-    'sphinx_rtd_theme',  # 仅保留必要扩展
+    'sphinx_rtd_theme',  # 确保已安装
 ]
 
-html_theme = 'sphinx_rtd_theme'  # 使用 RTD 主题
+html_theme = 'sphinx_rtd_theme'
 language = 'zh_CN'
