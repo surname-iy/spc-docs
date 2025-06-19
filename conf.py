72742c1d197ab2efdbec4a1488ad90b145c34cb1
surname-iy <827148614@qq.com>
2025-06-19 17:25:55 +0800

    “修复构建问题456”

--- conf.py
+++ conf.py
@@ -4,16 +4,14 @@ author = '你的名字'
 version = '1.0'
 release = '1.0'
 
-# 文档源目录（Sphinx 会从 docs/ 读取 RST 文件）
+# 显式设置文档源目录为 docs/
 source_dir = 'docs'  
-source_suffix = '.rst'
-
-# 主文档是 docs/index.rst（只需指定文件名，Sphinx 从 source_dir 查找）
+# 主文档是 docs/index.rst（只需写文件名，Sphinx 从 source_dir 查找）
 master_doc = 'index'  
 
+source_suffix = '.rst'
 extensions = [
     'sphinx_rtd_theme',  # 确保已安装
 ]
-
 html_theme = 'sphinx_rtd_theme'
 language = 'zh_CN'
